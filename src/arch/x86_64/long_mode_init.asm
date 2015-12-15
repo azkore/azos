3918934eb3a4b9e0a0237569ca010d205416418b
Eugene Korekin <azcore@gmail.com>
2015-12-15 02:09:21 +0000

    Base x86_64 baremetal

---
+++ src/arch/x86_64/long_mode_init.asm
@@ -0,0 +1,22 @@
+global long_mode_start
+
+section .text
+bits 64
+long_mode_start:
+    ; Blank out the screen
+    mov edi, 0xB8000
+    mov rcx, 500                      ; Since we are clearing uint64_t over here, we put the count as Count/4.
+    mov rax, 0x0F20F200F200F20       ; Set the value to set the screen to: black background, white foreground, blank spaces.
+    rep stosq                         ; Clear the entire screen. 
+
+.os_returned:
+    mov rax, 0x4f724f204f534f4f
+    mov [0xb8000], rax
+    mov rax, 0x4f724f754f744f65
+    mov [0xb8008], rax
+    mov rax, 0x4f214f644f654f6e
+    mov [0xb8010], rax
+    ;print `OKAY` to screen
+    mov rax, 0x2f592f412f4b2f4f
+    mov qword [0xb80A0], rax
+    hlt
